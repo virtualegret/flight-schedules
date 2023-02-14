01e7acba1f77a9734782537dad6b9e6614f27112
hydrogen-studio <hydrogen.studio.llc@gmail.com>
2023-02-13 22:24:45 -0800

    closer to stable but still beta

--- main.js
+++ main.js
@@ -1,27 +1,59 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, protocol, webContents } = require('electron');
 const { autoUpdater } = require('electron-updater');
+var fs = require('fs');
+var path = require('path');
+const isDev = require('electron-is-dev');
+
+
+protocol.registerSchemesAsPrivileged([{
+  scheme: 'app',
+  privileges: {
+      standard: true,
+      secure: true,
+      allowServiceWorkers: true,
+      supportFetchAPI: true
+  }
+}]);
 
 let mainWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    // width: 1000,
+    // height: 800,
+    width: 300,
+    height: 500,
+    autoHideMenuBar: true,
+    icon: __dirname + '/buildResources/icon.png',
     webPreferences: {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile('index.html');
+  if(isDev){
+    mainWindow.setSize(1000, 800);
+    mainWindow.loadFile('index.html');
+  }else{
+    mainWindow.loadFile('updateCheck.html');
+  }
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
 }
 
+let downloadPath = app.getPath("downloads");
+
 app.on('ready', () => {
+  protocol.registerFileProtocol('app', (request, callback) => {
+    const url = request.url.substr(6);
+    callback({
+        path: path.normalize(`${__dirname}/${url}`)
+    });
+}, (error) => {
+    if (error) console.error('Failed to register protocol');
+});
   createWindow();
+  mainWindow.setResizable(false);
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 app.on('window-all-closed', function () {
@@ -40,14 +72,36 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+
+autoUpdater.on('update-not-available', () => {
+  mainWindow.setSize(1000, 800);
+  mainWindow.loadFile('index.html');
 });
 
 autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+  autoUpdater.quitAndInstall();
 });
 
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
+ipcMain.on('readAirportImport', (event, file) => {
+  var data = fs.readFileSync(path.join(file[0]), 'utf8');
+  data = data.split("\n")
+  for(i in data){
+    data[i] = data[i].split(",")
+  }
+  event.sender.send('readAirportImport', { data: data });
+});
+
+ipcMain.on('exportAirport', (event, file) => {
+  file = JSON.parse(file);
+  for(i in file){
+    file[i] = file[i].join(",")
+  }
+  file = file.join("\n")
+  fs.writeFile(path.join(downloadPath, "./exportedAirport.csv"), file, 'utf8', function (err) {
+    if (err) {
+      event.sender.send('exportAirport', { error: true });
+    } else{
+      event.sender.send('exportAirport', { error: false });
+    }
+  });
 });
